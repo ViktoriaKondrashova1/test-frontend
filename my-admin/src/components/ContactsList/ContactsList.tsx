e703d22a32ddafb08b9db47d237e2f3318da3e0b
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-12 18:46:12 +0300

    feat: add thead styles

--- my-admin/src/components/ContactsList/ContactsList.tsx
+++ my-admin/src/components/ContactsList/ContactsList.tsx
@@ -1,12 +1,22 @@
 import { Datagrid, List, TextField } from "react-admin";
+import FilterSidebar from "../FilterSidebar/FilterSidebar";
+import "./ContactsList.scss";
 
 const ContactsList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <TextField source="company" />
+  <List aside={<FilterSidebar />}>
+    <Datagrid
+      rowClick="edit"
+      sx={{
+        "& .RaDatagrid-headerCell": {
+          backgroundColor: "#e7e8ef",
+          height: "48px",
+        },
+      }}
+    >
+      <TextField source="company" label="Full name" />
       <TextField source="job_title" />
       <TextField source="industry" />
-      <TextField source="country" />
+      <TextField source="country" label="Location" />
     </Datagrid>
   </List>
 );
