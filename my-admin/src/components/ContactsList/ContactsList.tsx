2418c3618c26ea78d9f9cae801644994f54331a0
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-13 14:04:28 +0300

    feat: add no results component

--- my-admin/src/components/ContactsList/ContactsList.tsx
+++ my-admin/src/components/ContactsList/ContactsList.tsx
@@ -1,15 +1,21 @@
 import { Datagrid, List, TextField } from "react-admin";
 import FilterSidebar from "../FilterSidebar/FilterSidebar";
+import NoResults from "../NoResults/NoResults";
 import "./ContactsList.scss";
 
 const ContactsList = () => (
   <List aside={<FilterSidebar />}>
     <Datagrid
+      bulkActionButtons={false}
       rowClick="edit"
+      empty={<NoResults />}
       sx={{
         "& .RaDatagrid-headerCell": {
           backgroundColor: "#e7e8ef",
           height: "48px",
+        },
+        "& .RaDatagrid-row": {
+          height: "56px",
         },
       }}
     >
