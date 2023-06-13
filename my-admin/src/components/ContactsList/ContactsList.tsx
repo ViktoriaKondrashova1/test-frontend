6f2420ab89e646b0b499d578d44984e045dabbfa
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-13 18:49:24 +0300

    feat: add custom name field

--- my-admin/src/components/ContactsList/ContactsList.tsx
+++ my-admin/src/components/ContactsList/ContactsList.tsx
@@ -2,36 +2,39 @@ import { Datagrid, List, TextField } from "react-admin";
 import FilterSidebar from "../FilterSidebar/FilterSidebar";
 import NoResults from "../NoResults/NoResults";
 import DetailsPanel from "../DetailsPanel/DetailsPanel";
+import NameField from "../NameField/NameField";
 import "./ContactsList.scss";
 
-const ContactsList = () => (
-  <List aside={<FilterSidebar />}>
-    <Datagrid
-      bulkActionButtons={false}
-      rowClick="expand"
-      expand={<DetailsPanel />}
-      isRowExpandable={(row) => row}
-      empty={<NoResults />}
-      className="contacts-table"
-      sx={{
-        "& .RaDatagrid-headerCell": {
-          backgroundColor: "#e7e8ef",
-          height: "48px",
-        },
-        "& .RaDatagrid-row": {
-          height: "56px",
-        },
-        "& .RaDatagrid-expandIcon": {
-          display: "none",
-        },
-      }}
-    >
-      <TextField source="company" label="Full name" />
-      <TextField source="job_title" />
-      <TextField source="industry" />
-      <TextField source="country" label="Location" />
-    </Datagrid>
-  </List>
-);
+const ContactsList = () => {
+  return (
+    <List aside={<FilterSidebar />}>
+      <Datagrid
+        bulkActionButtons={false}
+        rowClick="expand"
+        expand={<DetailsPanel />}
+        isRowExpandable={(row) => row}
+        empty={<NoResults />}
+        className="contacts-table"
+        sx={{
+          "& .RaDatagrid-headerCell": {
+            backgroundColor: "#e7e8ef",
+            height: "48px",
+          },
+          "& .RaDatagrid-row": {
+            height: "56px",
+          },
+          "& .RaDatagrid-expandIcon": {
+            display: "none",
+          },
+        }}
+      >
+        <NameField source="company" label="Full name" />
+        <TextField source="job_title" />
+        <TextField source="industry" />
+        <TextField source="country" label="Location" />
+      </Datagrid>
+    </List>
+  );
+};
 
 export default ContactsList;
