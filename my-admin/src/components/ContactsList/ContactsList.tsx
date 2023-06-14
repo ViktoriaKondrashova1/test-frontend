8236895fe1873e803ac97a57aab824a15f997521
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-14 14:29:35 +0300

    feat: add custom layout

--- my-admin/src/components/ContactsList/ContactsList.tsx
+++ my-admin/src/components/ContactsList/ContactsList.tsx
@@ -1,5 +1,4 @@
 import { Datagrid, List, TextField } from "react-admin";
-import FilterSidebar from "../FilterSidebar/FilterSidebar";
 import NoResults from "../NoResults/NoResults";
 import DetailsPanel from "../DetailsPanel/DetailsPanel";
 import NameField from "../NameField/NameField";
@@ -7,7 +6,7 @@ import "./ContactsList.scss";
 
 const ContactsList = () => {
   return (
-    <List aside={<FilterSidebar />}>
+    <List actions={false}>
       <Datagrid
         bulkActionButtons={false}
         rowClick="expand"
--- my-admin/src/pages/MainPage/MainPage.tsx
+++ my-admin/src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,11 @@
 import { Admin, Resource } from "react-admin";
 import dataProvider from "../../dataProvider";
 import ContactsList from "../../components/ContactsList/ContactsList";
+import CustomLayout from "../../components/CustomLayout/CustomLayout";
 
 const MainPage = () => {
   return (
-    <Admin dataProvider={dataProvider.getContacts}>
+    <Admin dataProvider={dataProvider.getContacts} layout={CustomLayout}>
       <Resource name="contacts" list={ContactsList} />
     </Admin>
   );
