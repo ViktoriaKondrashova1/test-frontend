b61747e2733fc13bfce48749b663af2cb70fc167
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-15 19:04:49 +0300

    feat: add no results component

--- my-admin/src/components/NoResults/NoResults.tsx
+++ my-admin/src/components/NoResults/NoResults.tsx
@@ -1,5 +1,17 @@
+import "./NoResults.scss";
+
 const NoResults = () => {
-  return <div>No Results</div>;
+  return (
+    <div className="no-results">
+      <div className="no-results__img" />
+      <h2 className="no-results__title">No results found</h2>
+      <p className="no-results__text">
+        We couldn’t find what you searched for. <br />
+        Please try again.
+      </p>
+      <button className="no-results__btn">Clear filters</button>
+    </div>
+  );
 };
 
 export default NoResults;
