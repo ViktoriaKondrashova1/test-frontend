355f3dfae871bc81f22d305a863c76fb7a89b20c
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-17 16:22:46 +0300

    feat: add healper and humber formatter

--- my-admin/src/components/CustomAppBar/CustomAppBar.tsx
+++ my-admin/src/components/CustomAppBar/CustomAppBar.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
+import { numberFormatter } from "../../helper";
 import { useDataProvider } from "react-admin";
 import "./CustomAppBar.scss";
 
 const CustomAppBar = () => {
   const dataProvider = useDataProvider();
-  const [total, setTotal] = useState<number | undefined>(0);
+  const [total, setTotal] = useState<number>(0);
   dataProvider
     .getList("contacts", {
       pagination: { page: 1, perPage: 10 },
       sort: { field: "job_title", order: "ASC" },
       filter: {},
     })
-    .then((data) => setTotal(data.total));
+    .then((data) => setTotal(data.total!));
   return (
     <div className="header">
       <div className="header__logo-wrap">
@@ -19,7 +20,7 @@ const CustomAppBar = () => {
       </div>
       <div className="header__total-wrap">
         <div className="header__total">Total</div>
-        <div className="header__total-count">{total}</div>
+        <div className="header__total-count">{numberFormatter(total)}</div>
       </div>
     </div>
   );
