ff6a5767aeaa38e71bc3928043789526c449095b
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-23 12:06:01 +0300

    feat: add toggleVisibility fn

--- my-admin/src/components/LoginForm/LoginForm.tsx
+++ my-admin/src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,7 @@
 import { useLogin, useNotify } from "react-admin";
 import { useState } from "react";
+import OpenEyeIcon from "../../assets/svg/icon-open-eye.svg";
+import ClosedEyeIcon from "../../assets/svg/icon-eye.svg";
 import "./LoginForm.scss";
 
 const LoginForm = () => {
@@ -11,6 +13,22 @@ const LoginForm = () => {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     login({ email, password }).catch(() => notify("Invalid email or password"));
+  };
+
+  const toggleVisibility = () => {
+    const passwordInput = document.querySelector(
+      ".password-input"
+    ) as HTMLInputElement;
+    const showBtn = document.querySelector(
+      ".login-form__show-btn"
+    ) as HTMLButtonElement;
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      showBtn.style.backgroundImage = `url(${OpenEyeIcon})`;
+    } else {
+      passwordInput.type = "password";
+      showBtn.style.backgroundImage = `url(${ClosedEyeIcon})`;
+    }
   };
 
   return (
@@ -29,7 +47,7 @@ const LoginForm = () => {
       <label htmlFor="password" className="login-form__label">
         Password
         <input
-          className="login-form__input"
+          className="login-form__input password-input"
           name="password"
           type="password"
           placeholder="Enter your password"
@@ -37,6 +55,11 @@ const LoginForm = () => {
           onChange={(e) => setPassword(e.target.value)}
         />
       </label>
+      <button
+        type="button"
+        className="login-form__show-btn"
+        onClick={toggleVisibility}
+      />
       <input type="submit" className="login-form__submit" value="Login" />
     </form>
   );
