c10b6ee09d07a683741ef8dc606e46f7edd6d5c8
ViktoriaKondrashova1 <kavidmi@gmail.com>
2023-06-29 18:40:21 +0300

    feat: add total formatter btn

--- my-admin/src/components/CustomAppBar/CustomAppBar.tsx
+++ my-admin/src/components/CustomAppBar/CustomAppBar.tsx
@@ -1,5 +1,7 @@
-import { Count, useListParams } from "react-admin";
+import { useGetList, useListParams } from "react-admin";
 import { useLocation } from "react-router-dom";
+import { numberFormatter } from "../../helper";
+import { useState } from "react";
 import "./CustomAppBar.scss";
 
 const CustomAppBar = () => {
@@ -7,20 +9,46 @@ const CustomAppBar = () => {
   const [listParams] = useListParams({
     resource: "contacts",
   });
+  const [isUnit, setIsUnit] = useState<boolean>(true);
+
+  const { total } = useGetList("contacts", {
+    pagination: { page: 1, perPage: 10 },
+    sort: { field: "company", order: "DESC" },
+    filter: listParams.filter,
+  });
 
   return (
     <header className="header">
       <div className="header__logo-wrap">
-        <h1 className="header__logo">logo</h1>
+        <h1
+          className="header__logo"
+          style={
+            location.pathname !== "/profile"
+              ? { borderRight: "1px solid #e7e8ef" }
+              : { borderBottom: "1px solid #e7e8ef" }
+          }
+        >
+          logo
+        </h1>
       </div>
-      {location.pathname !== "/profile" && (
-        <div className="header__total-wrap">
-          <div className="header__total">Total</div>
-          <div className="header__total-count">
-            {<Count resource="contacts" filter={listParams.filter} />}
+      <div className="header__wrap">
+        {location.pathname !== "/profile" && (
+          <div className="header__total-wrap">
+            <div className="header__total">Total</div>
+            <div className="header__total-count">
+              {isUnit ? numberFormatter(total!) : total}
+            </div>
           </div>
-        </div>
-      )}
+        )}
+        <button
+          className="header__btn"
+          onClick={() => {
+            isUnit ? setIsUnit(false) : setIsUnit(true);
+          }}
+        >
+          KM
+        </button>
+      </div>
     </header>
   );
 };
--- my-admin/src/helper.tsx
+++ my-admin/src/helper.tsx
@@ -3,5 +3,5 @@ export const numberFormatter = (num: number) => {
     return `${Math.sign(num) * Number((Math.abs(num) / 1000).toFixed(1))}K`;
   } else if (Math.abs(num) > 999999) {
     return `${Math.sign(num) * Number((Math.abs(num) / 1000000).toFixed(1))}M`;
-  } else return Math.sign(num) * Math.abs(num);
+  } else return (Math.sign(num) * Math.abs(num)).toString();
 };
